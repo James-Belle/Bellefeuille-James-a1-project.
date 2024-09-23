53a98f166107a6d1207a699d1ff22b7955198103
James Bellefeuille <spyro9046@outlook.com>
2024-09-23 17:30:51 -0400

    Added the beginning and outline of the ending

--- Bellefeuille-James-a1/Program.cs
+++ Bellefeuille-James-a1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Throughout this project my plan for handleing invalid input most of the time is to have an option for doing nothing
+// and treat the invalid input as if the playyer choose to do nothing 
+Console.WriteLine("");
+Console.WriteLine("");
+int suspicion = 0;
+string doorChoice = Console.ReadLine();
+if (doorChoice != "1")
+{
+    Console.WriteLine();
+}
+else // here the option is the same wether they chose to not open the door or there is invalid input.
+{
+    Console.WriteLine();
+    suspicion++;
+}
+Console.WriteLine();
+
+
+
+
+
+
+// Ending
+
+if (suspicion == 0)
+{
+
+}
+else if (suspicion < 3)
+{
+    
+}
+else if (suspicion <5)
+{
+    
+}
+else
+{
+
+}
+
+
+
